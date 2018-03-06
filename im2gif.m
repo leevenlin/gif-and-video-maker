19c33a54a6b7371c9d8d4767104d0653229120ca
leevenlin <venling@foxmail.com>
2018-03-06 13:30:47 +0800

    Update im2gif.m

--- im2gif.m
+++ im2gif.m
@@ -1,6 +1,6 @@
 
 %% images read
-inputdir = '../output/';
+inputdir = './';
 imgs = dir(strcat(inputdir,'*.jpg'));
 names = natsortfiles({imgs.name});
 
@@ -8,15 +8,15 @@ names = natsortfiles({imgs.name});
 num = length(names);
 
 %% gif write
-   for j=1:num
-        im=imread(strcat(inputdir,names{j}));
-        img = im(20:300,150:1400,:);
+for j=1:num
+     img=imread(strcat(inputdir,names{j}));
+     %img = im(20:300,150:1400,:);
      imshow(img)
-        
-         [I,map]=rgb2ind(img,20); 
-        if j==1
-           imwrite(I,map,'rrtnew.gif','gif', 'Loopcount',inf,'DelayTime',0.1);%FIRST
-       else
-           imwrite(I,map,'rrtnew.gif','gif','WriteMode','append','DelayTime',0.1);
-       end
-    end
+
+     [I,map]=rgb2ind(img,20); 
+     if j==1
+        imwrite(I,map,'rrtnew.gif','gif', 'Loopcount',inf,'DelayTime',0.1);%FIRST
+     else
+        imwrite(I,map,'rrtnew.gif','gif','WriteMode','append','DelayTime',0.1);
+     end
+ end
